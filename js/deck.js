f79cd734d4b253f034b001f71159f08eb754cdae
Cole <colemag383@gmail.com>
2019-09-08 14:12:22 +1200

    add routes

--- js/deck.js
+++ js/deck.js
@@ -1,4 +1,4 @@
-const { DeckGL, HexagonLayer } = deck;
+const { DeckGL, HexagonLayer, GeoJsonLayer } = deck;
 const KEY = '69bbd91448a04a79928312bda869d3c0'
 const deckgl = new DeckGL({
     mapboxApiAccessToken: 'pk.eyJ1IjoiY29sZS1tYWd1aXJlIiwiYSI6ImNrMDk1YWs3YzA1NnUzYnFsczI1eWMzNDcifQ.7KGfa-CJJdG-iTfoniHObg',
@@ -12,56 +12,49 @@ const deckgl = new DeckGL({
     minZoom: 5,
     maxZoom: 15,
     controller: true,
-    pitch: 40.5
+    pitch: 70
 });
 
 let data = null;
 
 const OPTIONS = ['radius', 'coverage'];
 
-const COLOR_RANGE = [
-    [1, 152, 189],
-    [73, 227, 206],
-    [216, 254, 181],
-    [254, 237, 177],
-    [254, 173, 84],
-    [209, 55, 78]
-];
 function renderLayer() {
-    const options = { radius: 5, coverage: 10 };
+    const options = { radius: 0.1, coverage: 500 };
 
     function lookupPoint(p) {
         return p.other.vehicle.occupancy_status
     }
 
+
     async function setTooltip(info) {
         const el = document.getElementById('tooltip');
-    
+        console.log(info)
         try {
             el.textContent = '...'
             el.style.display = 'block';
             el.style.left = info.x + 200 + 'px';
             el.style.top = info.y + 100 + 'px';
-            let resp = await fetch(`https://api.at.govt.nz/v2/gtfs/routes/routeId/${dataIn[info.index].other.vehicle.trip.route_id}`, {
+            let resp = await fetch(`https://api.at.govt.nz/v2/gtfs/routes/routeId/${data[info.index].other.vehicle.trip.route_id}`, {
                 headers: {
                     'Ocp-Apim-Subscription-Key': KEY
                 }
             })
             let content = await resp.json()
             el.textContent = `${content.response[0].route_short_name}: ${content.response[0].route_long_name}`;
-        } catch {
+        } catch (e) {
+            console.error(e)
             el.style.display = 'none';
         }
     }
 
-    function one(p){
+    function one(p) {
         return 1
     }
 
-    const hexagonLayer = new HexagonLayer({
-        id: 'heatmap',
-        colorRange: COLOR_RANGE,
-        getColorWeight:  document.getElementById("checkCongestion").checked? lookupPoint: one,
+    const busesLayer = new HexagonLayer({
+        layerName: 'heatmap',
+        getColorWeight: document.getElementById("checkCongestion").checked ? lookupPoint : one,
         data,
         pickable: true,
         onHover: async info => await setTooltip(info),
@@ -70,12 +63,29 @@ function renderLayer() {
         opacity: 1,
         ...options
     });
+
+    const routesLayer = new GeoJsonLayer({
+        layerName: 'line',
+        data: 'https://opendata.arcgis.com/datasets/d5a4db7acb5a45a9a4f1bd08a3f0f0a6_2.geojson',
+        pickable: false,
+        stroked: false,
+        filled: false,
+        extruded: false,
+        lineWidthScale: 20,
+        lineWidthMaxPixels: 3,
+        lineWidthMinPixels: 2,
+        getLineColor: [255, 255, 255, 128],
+        getRadius: 3,
+        getLineWidth: 1,
+        getElevation: 30,
+    })
     deckgl.setProps({
-        layers: [hexagonLayer]
+        layers: [routesLayer, busesLayer]
     });
 }
 
 let colors = {}
+
 const fetchData = () => {
     fetch(`https://api.at.govt.nz/v2/public/realtime/vehiclelocations`, {
         headers: {
